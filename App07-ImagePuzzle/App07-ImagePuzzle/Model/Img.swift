7b965f660d26c347dbd386c13951bc6c94926e96
A-Alafita <70872003+A-Alafita@users.noreply.github.com>
2021-11-05 14:57:23 -0600

    Add files via upload

---
+++ App07-ImagePuzzle/App07-ImagePuzzle/Model/Img.swift
@@ -0,0 +1,15 @@
+//
+//  Img.swift
+//  App07-ImagePuzzle
+//
+//  Created by David Cantú Delgado on 14/10/21.
+//
+
+import SwiftUI
+
+struct Img: Identifiable {
+    
+    var id: Int
+    var image: String    
+    
+}
